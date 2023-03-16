60f2e37c17bf5eaf39da3d5e454b0de53e8f02e1
eiiizong <eiiizong@163.com>
2023-03-16 21:05:40 +0800

    框架搭建

--- src/languages/index.ts
+++ src/languages/index.ts
@@ -1,13 +1,13 @@
 import { createI18n } from 'vue-i18n'
-
-// @ts-ignore：process未找到错误 => 需要该文件在vscode工作区的根目录下，才不会有错误提示。
-const { VITE_SYSTEM_LANGUAGE } = process.env
+import { getEnvData } from '@/utils/get'
 
 import messages from './locales'
 
+const systemLanguage = getEnvData('VITE_SYSTEM_LANGUAGE')
+
 const i18n = createI18n({
   legacy: false,
-  locale: VITE_SYSTEM_LANGUAGE || 'zh-cn',
+  locale: systemLanguage || 'zh-cn',
   missingWarn: false,
   fallbackWarn: false,
   messages
