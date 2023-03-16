e2247bf99c68b628e7c8950dfdc52656c7c79fab
eiiizong <eiiizong@163.com>
2023-03-16 15:27:29 +0800

    框架搭建

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -6,18 +7,54 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()]
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()]
-    }),
-    // 配置vite在运行的时候自动检测eslint规范
-    eslintPlugin({
-      include: ['src/**/*.ts', 'src/**/*.js', 'src/**/*.vue', 'src/*.ts', 'src/*.js', 'src/*.vue']
-    })
-  ]
+export default defineConfig(({ mode }) => {
+  // 本地变量
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    define: {
+      'process.env': env
+    },
+    css: {
+      /* CSS 预处理器 */
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "src/assets/styles/var.scss";'
+        }
+      },
+      devSourcemap: true
+    },
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      }),
+      // 配置vite在运行的时候自动检测eslint规范
+      eslintPlugin({
+        include: ['src/**/*.ts', 'src/**/*.js', 'src/**/*.vue', 'src/*.ts', 'src/*.js', 'src/*.vue']
+      })
+    ],
+    // 软链接
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '~': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    // 代理配置
+    server: {
+      port: 3333,
+      cors: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_REQUEST_URL,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, '')
+        }
+      }
+    }
+  }
 })
